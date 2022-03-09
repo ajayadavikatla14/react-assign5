935cf65ab39ee2ed37e32c72c766993d5811d680
ajayadavikatla14 <ajayadavikatla14@gmail.com>
2022-03-09 19:05:49 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
+import Contact from './Contact'
+import Navlinks from './Navlinks'
+import Students from './Students'
+
+const App = () => {
+  return (
+    <> 
+        <Router>
+            <Navlinks />
+            <Routes>
+                <Route path='/' element={<Home /> } />
+                <Route path='/students' element={<Students /> } />
+                <Route path='/students-desc' element={<Students /> } />
+                <Route path='/students-desc/:id' element={<Students /> } />
+                <Route path='/contact' element={<Contact /> } />
+                
+            </Routes>
+        </Router>
+    </>
+  )
+}
+
+const Home=()=>(
+    <div>
+        <h1>Home Page</h1>
+    </div>
+)
+
+export default App
